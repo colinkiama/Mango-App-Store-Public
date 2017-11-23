27fce30610cd93f5d994042d30963589393b2a7a
Colin Kiama <colinkiama@gmail.com>
2017-11-23 16:28:53 +0000

    First release of classes

---
+++ install.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Management.Deployment;
+
+namespace installTask
+{
+    public sealed class install : IBackgroundTask
+    {
+        BackgroundTaskDeferral _deferral;
+        string resultText = "Nothing";
+        bool pkgRegistered = false;
+        private static IBackgroundTaskInstance boom;
+        static double installPercentage = 0;
+        /// <summary>
+        /// Pretty much identical to showProgressInApp() in MainPage.xaml.cs
+        /// </summary>
+        /// <param name="taskInstance"></param>
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            boom = taskInstance;
+            _deferral = taskInstance.GetDeferral();
+            ApplicationTriggerDetails details = (ApplicationTriggerDetails)taskInstance.TriggerDetails;
+            string packagePath = "";
+
+            packagePath = (string)details.Arguments["packagePath"];
+            PackageManager pkgManager = new PackageManager();
+            Progress<DeploymentProgress> progressCallback = new Progress<DeploymentProgress>(installProgress);
+            notification.SendUpdatableToastWithProgress(0);
+           
+            
+                try
+                {
+                    var result = await pkgManager.AddPackageAsync(new Uri(packagePath), null, DeploymentOptions.None).AsTask(progressCallback);
+                    checkIfPackageRegistered(result, resultText);
+                }
+
+                catch (Exception e)
+                {
+                    resultText = e.Message;
+                }
+
+
+            
+
+
+            if (pkgRegistered == true)
+            {
+                notification.showInstallationHasCompleted();
+            }
+            else
+            {
+                notification.showError(resultText);
+            }
+
+
+            _deferral.Complete();
+        }
+
+
+        private static void installProgress(DeploymentProgress installProgress)
+        {
+             installPercentage = installProgress.percentage;
+            boom.Progress = (uint)installPercentage;
+            notification.UpdateProgress(installPercentage);
+        }
+
+
+
+
+        private void checkIfPackageRegistered(DeploymentResult result, string resultText)
+        {
+            if (result.IsRegistered)
+            {
+                pkgRegistered = true;
+            }
+            else
+            {
+                resultText = result.ErrorText;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ youtubeURLParsing.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mangoAppStore.Helpers
+{
+   public class youtubeURLParsing
+    {
+        public static string getYouTubeEmbedLink(string url)
+        {
+            string videoID = getVideoIDFromUrl(url);
+            string embedLinkToReturn = createEmbedLink(videoID);
+            return embedLinkToReturn;
+        }
+
+        public static string getYoutubeVideoID(string url)
+        {
+            string videoID = getVideoIDFromUrl(url);
+            return videoID;
+        }
+
+        private static string createEmbedLink(string videoID)
+        {
+            string embedLink = $"https://www.youtube.com/embed/{videoID}";
+            return embedLink;
+        }
+
+        private static string getVideoIDFromUrl(string url)
+        {
+
+            string videoID = "";
+
+
+            if (url.Contains("http"))
+            {
+                if (url.Contains("https"))
+                {
+                    url = url = url.Replace("https://", "");
+                }
+                else
+                {
+                    url = url.Replace("http://", "");
+                }
+            }
+
+
+            //and return nothing in final else
+
+
+
+            if (url.Contains("www"))
+            {
+                url = url.Replace("www.", "");
+            }
+
+            //Handle all m.youtube cases in one if
+            //all youtube cases in else if
+            //all youtu.be in another else if
+
+            if (url.Contains("m.youtube.com"))
+            {
+                if (url.Contains("m.youtube.com/embed"))
+                {
+                    videoID = url.Replace("m.youtube.com/embed/", "");
+                }
+                else
+                {
+                    videoID = url.Replace("m.youtube.com/watch?v=", "");
+                }
+            }
+
+            else if (url.Contains("youtube.com"))
+            {
+                if (url.Contains("youtube.com/embed"))
+                {
+                    videoID = url.Replace("youtube.com/embed/", "");
+                }
+                else
+                {
+                    videoID = url.Replace("youtube.com/watch?v=", "");
+                }
+
+            }
+
+
+
+
+
+
+
+
+            else if (url.Contains("youtu.be"))
+            {
+                videoID = url.Replace("youtu.be/", "");
+
+            }
+
+            else videoID = $"{url} is invalid.";
+
+            byte videoIDLength = (byte)videoID.Length;
+            if (videoID[videoIDLength - 2] == '?' && videoID[videoIDLength - 1] == 'a')
+            {
+                videoID = videoID.Remove(videoIDLength - 2, 2);
+            }
+
+            if (videoID.Contains("?autoplay"))
+            {
+                int startOfRemoval = videoID.LastIndexOf("?autoplay");
+                videoID = videoID.Remove(startOfRemoval);
+            }
+
+
+
+
+            return videoID;
+        }
+    }
+}
